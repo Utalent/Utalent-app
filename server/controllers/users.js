a63accfc1e3b71b79ef689f1d8636897d76863f1
AbobakrAlduais <aalduais@gmail.com>
2017-03-01 16:40:16 +0200

    user sign up

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,38 @@
-
+var jwt = require('jwt-simple');
+var User = require('./models/user.js');
 
 exports.modules = {
 	signin: function(){
 		//YOUR code here
+	} 
+}
+
+
+
+
+
+exports.modules = {
+	signup: function(req,res,next){
+		var username = req.body.username;
+    	var password = req.body.password;
+    	var email    = req.body.email
+
+    	findOne({ username: username })
+    		.then(function (err, user) {
+	    		if(user){
+	    			 next(new Error('User already exist!'));
+	    		}
+	    		else {
+	          // make a new user if not one
+	          return createUser({
+	            username: username,
+	            password: password,
+	            email   : email
+	          });
+	        	}
+
+    		})
+
+
 	} 
 }
---
+++ server/models/users.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var SALT_WORK_FACTOR = 10;
+
+
+var UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+  email : {
+    type: String,
+    required: true,
+    unique: true
+  },
+  salt: String
+});
