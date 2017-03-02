e25714e27f2857f6daa3b9dc84d72601ea7a4996
AbobakrAlduais <aalduais@gmail.com>
2017-03-02 11:58:34 +0200

    sign in & sign up

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -3,59 +3,51 @@ var util = require('../config/utils.js');
 var User = require('../models/users.js');
 
 module.exports = {
-	//  signin : function(req,res) {
- //  var email      = req.body.email; 
- //  var password   = req.body.password;
- //  var hashedpass = util.hashpass(password,function(hash){
- //      hashedpass = hash;
- //  });
- //    new User({ email: email }).fetch().then(function(found) {
- //    if (found) {
- //      var userHash = found.get('password');
- //      util.comparePass(password,userHash,function(exist){
- //        if(exist){
-       
- //          var token = jwt.encode(found, 'not your bussines!!');
- //          res.json({token: token});
- //                 }else{
- //          console.log("password is not correct")
- //            res.status(500).send("password is not correct")
+	 signin : function(req,res) {
+    var user = req.body;
 
- //        }
- //      })  
- //    } else {
- //    console.log("not found")
- //      }
- //  });
- // },
+    User.findOne({username: user.username})
+      .exec(function(err, found){
+        if(found){
+          util.comparePass(user.password, found.password,function(exist){
+            if(exist){
+            var token = jwt.encode(found, 'not your bussines!!');
+            res.json({token: token});
+            }
+            else{
+            console.log("password is not correct")
+            res.status(500).send("Password is not correct")
+            }
+          })  
+        }
+        else{
+          res.status(500).send("User not found!")
+        }
+      })
+ },
 
 
 
 signup: function(req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    var hashedpass = util.hashpass(password,function(hash){
-          hashedpass = hash;
-      	})
+    var user = req.body;
+    
+    util.hashpass(user.password,function(hash){
+          user.hashedpass = hash;
+    })
    
-    var email = req.body.email;
     // check to see if user already exists
-    User.findOne({username: username})
-      .exec(function (err, user) {
-        if (user) {
+    User.findOne({username: user.username})
+      .exec(function (err, found) {
+        if (found) {
           res.json('User already exist!');
         } else {
           // make a new user if not one
-          return User.create({
-            username: username,
-            password: hashedpass,
-            email:email,
-          }, function (err, newUser) {
+          return User.create(user, function (err, newUser) {
               // create token to send back for auth
               if(err){
                 res.json(err);
               } else {
-                var token = jwt.encode(user, 'secret');
+                var token = jwt.encode(found, 'secret');
                 res.json({token: token}); 
               }     
           });
