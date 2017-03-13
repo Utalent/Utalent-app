8425ec97392f0a5478afa631da01b2ec9dccf337
duaa maloalain <maloalain.duaa94@hotmail.com>
2017-03-13 17:27:54 +0200

    (add) add token

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-signup',
@@ -13,8 +14,10 @@ export class SignupComponent implements OnInit {
 
 
  user ={};
+ // localStorageService: LocalStorageService;
 
-  constructor (private authService: AuthService) { }
+  constructor (
+    private authService: AuthService) { }
  
 
   ngOnInit() {
@@ -23,10 +26,11 @@ export class SignupComponent implements OnInit {
    
   signup() {
     console.log("ttttttttttt",this.user)
-    this.authService.signup(this.user).subscribe(user => {
-    
-    });
-
-}
-
+    this.authService.signup(this.user).subscribe(data => {
+        if (data){
+          localStorage.setItem('com.utalent',JSON.stringify(data.token))
+          localStorage.setItem('com.userId',JSON.stringify(data.id))
+        }
+    })
+  }
 }
