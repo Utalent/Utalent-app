2572d3cf2e2b190d53c11b1798ff884a521a74fd
duaa maloalain <maloalain.duaa94@hotmail.com>
2017-03-16 10:55:56 +0200

    (feat) add profile component add getphoto and add photo function

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { ProfileService } from '../profile.service';
+import {  ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+ 
+ image:any;
+  photo:any;
+  constructor(private profileService: ProfileService,private changeDetectorRef: ChangeDetectorRef, private route: ActivatedRoute ) {
+   }
+  
+  challenge = {};
+  private sub;
 
   ngOnInit() {
+    this.getPhoto()
   }
+  
+    getPhoto(){
+    this.sub = this.route.params.subscribe(params => {
+         let username= params['username'];
+      this.name=username
+        this.profileService.getphoto({username: username}).subscribe(image=>
+        this.photo=image);
+    })
+
+  }
+
+  // upload image start
+    fileChange(input){
+    this.readFiles(input.files);
+      console.log(this.image ,"iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+    
+  }
+  readFile(file, reader, callback){
+    // Set a callback funtion to fire after the file is fully loaded
+    reader.onload = () => {
+      // callback with the results
+      // console.log(reader.result)
+     callback(reader.result);
+
+    }
+    
+    // Read the file
+    reader.readAsDataURL(file);
+  }
+ 
+
+
+ readFiles(files){
+    // Create the file reader
+    let reader = new FileReader();
+    
+    // If there is a file
+    if (files[0]){
+      // Start reading this file
+      this.readFile(files[0], reader, (result) =>{
+        // Create an img element and add the image file data to it
+      this.image=result; 
+      this.Addphoto(result)
+      // console.log(result);
+      });
+       
+    }else{
+      // When all files are done This forces a change detection
+      this.changeDetectorRef.detectChanges();
+    }
+  }
+  // upload image end 
+
+name:any;
+
+  Addphoto(image){
+    this.sub = this.route.params.subscribe(params => {
+       let username= params['username'];
+this.name=username
+      console.log(username)
+      this.profileService.Addphoto({image:image, username:username}).subscribe(d=>{
+      });          
+    })
+  }
+
+ 
+  
+
+  
+  ngOnDestroy() {
+      this.sub.unsubscribe();
+  }
+
 
 }
