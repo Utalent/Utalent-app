7fa040cf6e011184837feb6776e82f3a08e9bffa
duaa maloalain <maloalain.duaa94@hotmail.com>
2017-03-16 10:53:47 +0200

    (add) add profile component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { AuthService } from './auth.service';
 import { InterestsService } from './interests.service';
 
 import { ProfileComponent } from './profile/profile.component';
+import { ProfileService } from './profile.service';
 
 
 import { ChallengeComponent } from './challenge/challenge.component';
@@ -45,11 +46,10 @@ const ROUTES = [
     path: 'interest',
     component: InterestComponent
   },
-  //  {
-  //   path: 'users/:username',
-  //   component: profile
-  // }
-
+   {
+    path: 'users/:username',
+    component: ProfileComponent 
+  },
   {
   path: 'challenges/:id',
   component: ChallengeComponent
@@ -61,7 +61,6 @@ const ROUTES = [
     SigninComponent,
     SignupComponent,
     InterestComponent,
-
     ChallengeComponent,
     ProfileComponent
 
@@ -76,7 +75,7 @@ const ROUTES = [
         }),
     RouterModule.forRoot(ROUTES) 
   ],
-  providers: [AuthService,InterestsService,ChallengeService,{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  providers: [AuthService,InterestsService,ProfileService,ChallengeService,{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
