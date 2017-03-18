e891ef7a7a801ade75ec575d4afbd4f9bb8f622f
duaa maloalain <maloalain.duaa94@hotmail.com>
2017-03-18 15:09:42 +0200

    (add ) add function

--- src/app/interest-user/interest-user.component.ts
+++ src/app/interest-user/interest-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InterestUserService } from '../interest-user.service';
+import {  ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-interest-user',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterestUserComponent implements OnInit {
 
-  constructor() { }
+  interest:any; 
+
+  constructor(private route: ActivatedRoute, private interestUserService : InterestUserService ) { }
+    private sub;
 
   ngOnInit() {
   }
+
+
+  GetUserInterests(){
+	  this.sub = this.route.params.subscribe(params => {
+	  	let userId= params['user_id'];
+	  	// this.name=username
+	  	this.interestUserService.getUserInterests({user_id: userId}).subscribe(interest=>
+	  		this.interest=interest);
+	  })
+  }  
+
 
 }
