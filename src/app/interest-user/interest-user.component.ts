865549d89ff210085e516fc261dc777784791fd6
duaa maloalain <maloalain.duaa94@hotmail.com>
2017-03-18 12:36:34 +0200

    (add) add user-interest component

---
+++ src/app/interest-user/interest-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interest-user',
+  templateUrl: './interest-user.component.html',
+  styleUrls: ['./interest-user.component.css']
+})
+export class InterestUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
