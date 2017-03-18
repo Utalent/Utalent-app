62afbcaf63e3e9d7c170116c76dc0bb7e6dc8a1c
HadeelBaloush <hadeelbaloush@gmail.com>
2017-03-18 10:42:41 +0400

    h

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ let app = express();
 let server = require("http").createServer(app);
 
 
-<<<<<<< HEAD
 let port = process.env.PORT || 4000;
-=======
-let port = process.env.PORT || 3000;
->>>>>>> b15818612d8c94538bd5a54e98cfcaa071e1db43
+
 ///////////////////////////////fb///////////////////////////////
 // let passport_fb = require('passport');
 // let passport    = require('./config/passport');
