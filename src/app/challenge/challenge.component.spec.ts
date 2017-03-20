14496230140a134b19fdf6b3c37990c2802fea5c
duaa maloalain <maloalain.duaa94@hotmail.com>
2017-03-20 10:00:48 +0200

    (feat) add test for challenge component

--- src/app/challenge/challenge.component.spec.ts
+++ src/app/challenge/challenge.component.spec.ts
@@ -1,28 +1,39 @@
-// /* tslint:disable:no-unused-variable */
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { DebugElement } from '@angular/core';
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-// import { ChallengeComponent } from './challenge.component';
+import { ChallengeComponent } from './challenge.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { ChallengeService } from '../challenge.service';
 
-// describe('ChallengeComponent', () => {
-//   let component: ChallengeComponent;
-//   let fixture: ComponentFixture<ChallengeComponent>;
+import { Http } from '@angular/http';
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ ChallengeComponent ]
-//     })
-//     .compileComponents();
-//   }));
+describe('ChallengeComponent', () => {
+  let component: ChallengeComponent;
+  let fixture: ComponentFixture<ChallengeComponent>;
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(ChallengeComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ChallengeComponent ],
+      providers: [ ChallengeService,
+            {provide: RouterTestingModule,  useClass:  ChallengeComponent},
+                   {provide: Http, useValue:ChallengeService  }
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    	],
+        imports: [ RouterTestingModule,FormsModule  ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChallengeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
