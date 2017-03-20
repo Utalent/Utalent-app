278736daffb5a01786f622951074eb21a4379d4d
duaa maloalain <maloalain.duaa94@hotmail.com>
2017-03-20 12:08:43 +0200

    (fix) fix test

--- src/app/signin/signin.component.spec.ts
+++ src/app/signin/signin.component.spec.ts
@@ -2,8 +2,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
-
 import { SigninComponent } from './signin.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
