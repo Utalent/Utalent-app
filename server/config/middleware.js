60fc8e1fa76f8ee2ee12ff86cfc18b7955214d8e
duaa maloalain <maloalain.duaa94@hotmail.com>
2017-03-20 12:03:23 +0200

    (fix)error

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -12,12 +12,12 @@ module.exports = function(app, express){
     });
 
     app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: true,limit: '50mb' }));
-    // app.use(bodyParser.json());
-    app.use(bodyParser.json({limit: '50mb'}));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
     ////////////////to upload any image at any size/////////////
   
-    // app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true,limit: '50mb' }));
+    app.use(bodyParser.json({limit: '50mb'}));
 
 
     ////////////////
