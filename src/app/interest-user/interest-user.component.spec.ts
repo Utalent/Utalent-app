8a4208247632af7318d2199016fdc10c33b26156
duaa maloalain <maloalain.duaa94@hotmail.com>
2017-03-20 10:02:47 +0200

    (feat) add test for interest-user component

--- src/app/interest-user/interest-user.component.spec.ts
+++ src/app/interest-user/interest-user.component.spec.ts
@@ -1,28 +1,40 @@
-// /* tslint:disable:no-unused-variable */
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { DebugElement } from '@angular/core';
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-// import { InterestUserComponent } from './interest-user.component';
+import { InterestUserComponent } from './interest-user.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { InterestUserService } from '../interest-user.service';
+import { Http } from '@angular/http';
 
-// describe('InterestUserComponent', () => {
-//   let component: InterestUserComponent;
-//   let fixture: ComponentFixture<InterestUserComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ InterestUserComponent ]
-//     })
-//     .compileComponents();
-//   }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(InterestUserComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+describe('InterestUserComponent', () => {
+  let component: InterestUserComponent;
+  let fixture: ComponentFixture<InterestUserComponent>;
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ InterestUserComponent ],
+      providers: [ InterestUserService,
+            {provide: RouterTestingModule,  useClass: InterestUserComponent},
+                   {provide: Http, useValue:InterestUserService  }
+
+    	],
+        imports: [ RouterTestingModule,FormsModule  ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InterestUserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
