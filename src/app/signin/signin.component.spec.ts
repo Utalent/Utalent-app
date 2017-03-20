051fdf610e85debaa9e9fbace0d08186ed788082
duaa maloalain <maloalain.duaa94@hotmail.com>
2017-03-20 10:05:55 +0200

    (feat) add test for signin component

--- src/app/signin/signin.component.spec.ts
+++ src/app/signin/signin.component.spec.ts
@@ -1,28 +1,41 @@
-// /* tslint:disable:no-unused-variable */
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { DebugElement } from '@angular/core';
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-// import { SigninComponent } from './signin.component';
 
-// describe('SigninComponent', () => {
-//   let component: SigninComponent;
-//   let fixture: ComponentFixture<SigninComponent>;
+import { SigninComponent } from './signin.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Http } from '@angular/http';
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ SigninComponent ]
-//     })
-//     .compileComponents();
-//   }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(SigninComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+describe('SigninComponent', () => {
+  let component: SigninComponent;
+  let fixture: ComponentFixture<SigninComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SigninComponent ],
+       providers: [ AuthService,
+            {provide: RouterTestingModule,  useClass: SigninComponent},
+                   {provide: Http, useValue:AuthService  }
+
+    	],
+        imports: [ RouterTestingModule,FormsModule  ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SigninComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
