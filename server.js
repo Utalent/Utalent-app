4ec530792650da9e5fcfd4dc631ac4a4dcb80187
AhmadKhasawneh <Ahmad.Khasawneh@hotmail.com>
2017-03-22 14:47:34 +0200

    (fix) package.json

--- server.js
+++ server.js
@@ -14,18 +14,18 @@ let port = process.env.PORT || 3000;
 // app.use(passport.initialize());
 // app.use(passport.session());
 
-
-require("./server/config/middleware.js") (app,express);
-require("./server/config/routes.js") (app,express);
-
-
-///////////////////////database////////////////////////
 let mongoURI = process.env.MONGODB_URI ||"mongodb://localhost/utalent";
 mongoose.connect(mongoURI);
 db = mongoose.connection;
 db.once("open", () => {
 	console.log("mongoDB is open");
 });
+
+require("./server/config/middleware.js") (app,express);
+require("./server/config/routes.js") (app,express);
+
+
+///////////////////////database////////////////////////
 
 
 
